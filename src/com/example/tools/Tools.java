3416557ba7a59503a197e579d1f0faefdfa063c7
jychen <ion9210cjy@163.com>
2017-06-27 11:45:12 +0800

    Update Tools.java

--- src/com/example/tools/Tools.java
+++ src/com/example/tools/Tools.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
 
 public class Tools {
 
+	//Test the change in github, git pull
 	public Context context;
 	public Tools(Context context){
 		this.context = context;
